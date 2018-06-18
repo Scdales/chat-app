69eaf4bed339282f8b2f8c773028ecc98767d838
Sean <seancliffdaley@gmail.com>
2018-06-18 20:43:05 +0100

    io.emit new messages

--- server/server.js
+++ server/server.js
@@ -13,15 +13,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    
-    socket.emit('newMessage', { //Object second argument
-        from: 'server user',
-        text: 'Yo whassup',
-        createdAt: 123
-    });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createEmail', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
